24e2077762730581327ab73ef9ad60f82f333d06
John Moore <john.moore@snh.gov.uk>
2021-01-30 18:34:53 +0000

    Add models
    Issue: Scottish-Natural-Heritage/Licensing#527

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,8 @@ import dbConfig from '../config/database.js';
 import Sequelize from 'sequelize';
 
 import Registration from './registration.js';
+import Return from './return.js';
+import NonTargetSpecies from './non-target-species.js';
 
 const sequelize = new Sequelize(dbConfig.database);
 
@@ -9,5 +11,10 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 db.Registration = Registration(sequelize, Sequelize);
+db.Return = Return(sequelize, Sequelize);
+db.NonTargetSpecies = NonTargetSpecies(sequelize, Sequelize);
+
+db.Return.hasMany(db.NonTargetSpecies);
+db.NonTargetSpecies.belongsTo(db.Return);
 
 export {db as default};
---
+++ src/models/return.js
@@ -0,0 +1,39 @@
+'use strict';
+
+import Sequelize from 'sequelize';
+
+const {Model} = Sequelize;
+
+/**
+ * Build an Return model.
+ *
+ * @param {Sequelize.Sequelize} sequelize A Sequelize connection.
+ * @returns {Sequelize.Model} An Return model.
+ */
+const ReturnModel = (sequelize) => {
+  class Return extends Model {}
+
+  Return.init(
+    {
+      trapRegistrationNumber: {
+        type: Sequelize.INTEGER,
+        validate: {
+          notEmpty: true
+        }
+      },
+      nonTargetSpeciesToReport: {
+        type: Sequelize.BOOLEAN
+      }
+    },
+    {
+      sequelize,
+      modelName: 'Return',
+      timestamps: true,
+      paranoid: true
+    }
+  );
+
+  return Return;
+};
+
+export {ReturnModel as default};
