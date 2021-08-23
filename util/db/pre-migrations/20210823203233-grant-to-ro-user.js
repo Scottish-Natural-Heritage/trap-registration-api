9a5857c8736f911c66f58d779a37734e2dda0761
Paul Cox <paul.cox@nature.scot>
2021-08-23 21:01:43 +0100

    Add premigration scripts to create ro role and select only permissions. Issue Lice 811

---
+++ util/db/pre-migrations/20210823203233-grant-to-ro-user.js
@@ -0,0 +1,57 @@
+'use strict';
+
+// The pre-migrations only make sense when running inside the production docker
+// environment. They are not required for the development SQLite DB.
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    up: async (queryInterface, Sequelize) => {
+      await queryInterface.sequelize.query('grant connect on database licensing to rotraps;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query('grant usage on schema traps to rotraps;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query('grant select on all tables in schema traps to rotraps;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query(
+        'alter default privileges for role licensing, traps in schema traps grant select on tables to rotraps;',
+        {
+          type: Sequelize.QueryTypes.RAW
+        }
+      );
+    },
+    down: async (queryInterface, Sequelize) => {
+      await queryInterface.sequelize.query(
+        'alter default privileges for role licensing, traps in schema traps revoke select on tables to rotraps;',
+        {
+          type: Sequelize.QueryTypes.RAW
+        }
+      );
+
+      await queryInterface.sequelize.query('revoke select on schema traps from rotraps;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query('revoke usage on schema traps from rotraps;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query('revoke all on database licensing from rotraps;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+    }
+  };
+} else {
+  module.exports = {
+    up: () => {
+      return Promise.resolve();
+    },
+    down: () => {
+      return Promise.resolve();
+    }
+  };
+}
