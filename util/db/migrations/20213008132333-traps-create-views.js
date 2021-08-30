b160d4926ce9b20173f2368c601e5a1a551d15e1
Paul Cox <paul.cox@nature.scot>
2021-08-30 14:16:01 +0100

    Fix file name and lint errors. Issue Licensing 830

--- util/db/migrations/20213008132333-traps-create-views.js
+++ util/db/migrations/20213008132333-traps-create-views.js
@@ -3,13 +3,19 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     up: async (queryInterface, Sequelize) => {
-      await queryInterface.sequelize.query('CREATE VIEW non_target_species_traps_v AS SELECT * FROM traps."NonTargetSpecies";', {
-        type: Sequelize.QueryTypes.RAW
-      });
+      await queryInterface.sequelize.query(
+        'CREATE VIEW non_target_species_traps_v AS SELECT * FROM traps."NonTargetSpecies";',
+        {
+          type: Sequelize.QueryTypes.RAW
+        }
+      );
 
-      await queryInterface.sequelize.query('CREATE VIEW registrations_traps_v AS SELECT * FROM traps."Registrations";', {
-        type: Sequelize.QueryTypes.RAW
-      });
+      await queryInterface.sequelize.query(
+        'CREATE VIEW registrations_traps_v AS SELECT * FROM traps."Registrations";',
+        {
+          type: Sequelize.QueryTypes.RAW
+        }
+      );
 
       await queryInterface.sequelize.query('CREATE VIEW returns_traps_v AS SELECT * FROM traps."Returns";', {
         type: Sequelize.QueryTypes.RAW
@@ -18,7 +24,6 @@ if (process.env.NODE_ENV === 'production') {
       await queryInterface.sequelize.query('CREATE VIEW revocations_traps_v AS SELECT * FROM traps."Revocations";', {
         type: Sequelize.QueryTypes.RAW
       });
-
     },
     down: async (queryInterface, Sequelize) => {
       await queryInterface.sequelize.query('DROP VIEW revocations_traps_v;', {
