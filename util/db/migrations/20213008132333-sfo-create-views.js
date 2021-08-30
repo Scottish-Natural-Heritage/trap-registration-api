a670e418b8678291ec8bfa95c8b2ff866ac3a912
Paul Cox <paul.cox@nature.scot>
2021-08-30 13:50:01 +0100

    Add migration script to create traps views for superset. Issue Lice 830

---
+++ util/db/migrations/20213008132333-sfo-create-views.js
@@ -0,0 +1,50 @@
+'use strict';
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    up: async (queryInterface, Sequelize) => {
+      await queryInterface.sequelize.query('CREATE VIEW non_target_species_traps_v AS SELECT * FROM traps."NonTargetSpecies";', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query('CREATE VIEW registrations_traps_v AS SELECT * FROM traps."Registrations";', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query('CREATE VIEW returns_traps_v AS SELECT * FROM traps."Returns";', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query('CREATE VIEW revocations_traps_v AS SELECT * FROM traps."Revocations";', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+    },
+    down: async (queryInterface, Sequelize) => {
+      await queryInterface.sequelize.query('DROP VIEW revocations_traps_v;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query('DROP VIEW returns_traps_v;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query('DROP VIEW registrations_traps_v;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query('DROP VIEW non_target_species_traps_v;', {
+        type: Sequelize.QueryTypes.RAW
+      });
+    }
+  };
+} else {
+  module.exports = {
+    up: () => {
+      return Promise.resolve();
+    },
+    down: () => {
+      return Promise.resolve();
+    }
+  };
+}
