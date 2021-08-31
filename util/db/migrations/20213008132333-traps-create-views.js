62349333cfc89a883ba2d33e9d42af7edc3db1e2
Paul Cox <paul.cox@nature.scot>
2021-08-31 09:57:13 +0100

    Update view names. Issue Licensing 830

--- util/db/migrations/20213008132333-traps-create-views.js
+++ util/db/migrations/20213008132333-traps-create-views.js
@@ -4,41 +4,38 @@ if (process.env.NODE_ENV === 'production') {
   module.exports = {
     up: async (queryInterface, Sequelize) => {
       await queryInterface.sequelize.query(
-        'CREATE VIEW non_target_species_traps_v AS SELECT * FROM traps."NonTargetSpecies";',
+        'CREATE VIEW traps_NonTargetSpecies AS SELECT * FROM traps."NonTargetSpecies";',
         {
           type: Sequelize.QueryTypes.RAW
         }
       );
 
-      await queryInterface.sequelize.query(
-        'CREATE VIEW registrations_traps_v AS SELECT * FROM traps."Registrations";',
-        {
-          type: Sequelize.QueryTypes.RAW
-        }
-      );
-
-      await queryInterface.sequelize.query('CREATE VIEW returns_traps_v AS SELECT * FROM traps."Returns";', {
+      await queryInterface.sequelize.query('CREATE VIEW traps_Registrations AS SELECT * FROM traps."Registrations";', {
         type: Sequelize.QueryTypes.RAW
       });
 
-      await queryInterface.sequelize.query('CREATE VIEW revocations_traps_v AS SELECT * FROM traps."Revocations";', {
+      await queryInterface.sequelize.query('CREATE VIEW traps_Returns AS SELECT * FROM traps."Returns";', {
+        type: Sequelize.QueryTypes.RAW
+      });
+
+      await queryInterface.sequelize.query('CREATE VIEW traps_Revocations AS SELECT * FROM traps."Revocations";', {
         type: Sequelize.QueryTypes.RAW
       });
     },
     down: async (queryInterface, Sequelize) => {
-      await queryInterface.sequelize.query('DROP VIEW revocations_traps_v;', {
+      await queryInterface.sequelize.query('DROP VIEW traps_Revocations;', {
         type: Sequelize.QueryTypes.RAW
       });
 
-      await queryInterface.sequelize.query('DROP VIEW returns_traps_v;', {
+      await queryInterface.sequelize.query('DROP VIEW traps_Returns;', {
         type: Sequelize.QueryTypes.RAW
       });
 
-      await queryInterface.sequelize.query('DROP VIEW registrations_traps_v;', {
+      await queryInterface.sequelize.query('DROP VIEW traps_Registrations;', {
         type: Sequelize.QueryTypes.RAW
       });
 
-      await queryInterface.sequelize.query('DROP VIEW non_target_species_traps_v;', {
+      await queryInterface.sequelize.query('DROP VIEW traps_NonTargetSpecies;', {
         type: Sequelize.QueryTypes.RAW
       });
     }
