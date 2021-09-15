bb09374d09d571cbc2c0534210eda2abf9bd6051
Paul Cox <paul.cox@nature.scot>
2021-09-15 14:39:01 +0100

    Get update expiry date migration script to work. Issue Licensing 832

--- util/db/migrations/20210915085630-update-expiry-date-column.js
+++ util/db/migrations/20210915085630-update-expiry-date-column.js
@@ -3,13 +3,10 @@ const databaseConfig = require('../../../src/config/database.js');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // Grab the id and createdAt value of any existing registration.
-    const resultsArray = await queryInterface.sequelize.query(
-      'SELECT * FROM Registrations;',
-      {
-        type: Sequelize.QueryTypes.RAW
-      }
-    );
+    // Grab the registrations as an array of objects.
+    const resultsArray = await queryInterface.sequelize.query('SELECT * FROM Registrations;', {
+      type: Sequelize.QueryTypes.SELECT
+    });
 
     // Loop through the results and add an expiry date value, calculated from the createdAt field.
     for (const result of resultsArray) {
@@ -23,16 +20,29 @@ module.exports = {
 
     // Loop through the updated results and update the expiryDate field with the new value.
     for (const result of resultsArray) {
-      await queryInterface.sequelize.query(
-        `UPDATE Registrations SET expiryDate = ? WHERE id = ?;`,
-        {
-          replacements: [result.expiryDate, result.id],
-          type: Sequelize.QueryTypes.RAW
-        }
-      );
+      await queryInterface.sequelize.query(`UPDATE Registrations SET expiryDate = ? WHERE id = ?;`, {
+        replacements: [result.expiryDate, result.id],
+        type: Sequelize.QueryTypes.UPDATE
+      });
     }
   },
   down: async (queryInterface, Sequelize) => {
     // Put the opposite here.
+    const resultsArray = await queryInterface.sequelize.query('SELECT * FROM Registrations;', {
+      type: Sequelize.QueryTypes.SELECT
+    });
+
+    // Loop through the results and set the expiry date to null.
+    for (const result of resultsArray) {
+      result.expiryDate = null;
+    }
+
+    // Loop through the updated results and update the expiryDate field with null.
+    for (const result of resultsArray) {
+      await queryInterface.sequelize.query(`UPDATE Registrations SET expiryDate = ? WHERE id = ?;`, {
+        replacements: [result.expiryDate, result.id],
+        type: Sequelize.QueryTypes.UPDATE
+      });
+    }
   }
 };
