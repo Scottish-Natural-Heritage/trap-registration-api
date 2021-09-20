549ef47c78a8a4f463d178ada1d1605b456c5315
Paul Cox <paul.cox@nature.scot>
2021-09-20 08:29:55 +0100

    Add get single registration v2 endpoint. Issue Licensing 880 885

--- src/v2-router.js
+++ src/v2-router.js
@@ -114,7 +114,22 @@ v2Router.post('/registrations', async (request, response) => {
  * READs a single registration.
  */
 v2Router.get('/registrations/:id', async (request, response) => {
-  return response.status(501).send({message: 'Not implemented.'});
+  try {
+    const existingId = Number(request.params.id);
+    if (Number.isNaN(existingId)) {
+      return response.status(404).send({message: `Registration ${request.params.id} not valid.`});
+    }
+
+    const registration = await Registration.findOne(existingId);
+
+    if (registration === undefined || registration === null) {
+      return response.status(404).send({message: `Registration ${request.params.id} not valid.`});
+    }
+
+    return response.status(200).send(registration);
+  } catch (error) {
+    return response.status(500).send({error});
+  }
 });
 
 /**
