aa4e8df229c29801cd6c2580c6a089a5d137e628
John Moore <john.moore@snh.gov.uk>
2021-09-21 11:14:45 +0100

    Add migration to fix broken child records
    Issue: Scottish-Natural-Heritage/Licensing#775

---
+++ util/db/migrations/20210921091114-traps-fix-any-broken-child-records-with-deleted-parents.js
@@ -0,0 +1,50 @@
+'use strict';
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    up: async (queryInterface, Sequelize) => {
+      await queryInterface.sequelize.query(
+        `
+        UPDATE traps."Returns" SET "deletedAt" = CURRENT_TIMESTAMP WHERE "deletedAt" IS NULL AND "RegistrationId" IN (SELECT id FROM traps."Registrations" WHERE "deletedAt" IS NOT NULL)`,
+        {
+          type: Sequelize.QueryTypes.UPDATE
+        }
+      );
+
+      await queryInterface.sequelize.query(
+        `
+        UPDATE traps."NonTargetSpecies" SET "deletedAt" = CURRENT_TIMESTAMP WHERE "deletedAt" IS NULL AND "ReturnId" IN (SELECT id FROM traps."Returns" WHERE "deletedAt" IS NOT NULL)`,
+        {
+          type: Sequelize.QueryTypes.UPDATE
+        }
+      );
+    },
+
+    down: () => {
+      return Promise.resolve();
+    }
+  };
+} else {
+  module.exports = {
+    up: async (queryInterface, Sequelize) => {
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "Returns" SET "deletedAt" = CURRENT_TIMESTAMP WHERE "deletedAt" IS NULL AND "RegistrationId" IN (SELECT id FROM "Registrations" WHERE "deletedAt" IS NOT NULL)`,
+        {
+          type: Sequelize.QueryTypes.UPDATE
+        }
+      );
+
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "NonTargetSpecies" SET "deletedAt" = CURRENT_TIMESTAMP WHERE "deletedAt" IS NULL AND "ReturnId" IN (SELECT id FROM "Returns" WHERE "deletedAt" IS NOT NULL)`,
+        {
+          type: Sequelize.QueryTypes.UPDATE
+        }
+      );
+    },
+
+    down: () => {
+      return Promise.resolve();
+    }
+  };
+}
