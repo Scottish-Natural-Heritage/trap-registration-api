b268872d95f06926e6c6441febbfe2cfd3b6fbde
Shazreen Seatter <71439994+ShazSeatter@users.noreply.github.com>
2024-11-15 15:18:11 +0000

    Add sendRenewalReminderEmail method Issue: Scottish-Natural-Heritage/Licensing#2434

--- src/controllers/v2/scheduled.js
+++ src/controllers/v2/scheduled.js
@@ -1,4 +1,5 @@
 import NotifyClient from 'notifications-node-client';
+import {Op} from 'sequelize';
 import database from '../../models/index.js';
 import config from '../../config/app.js';
 import jsonConsoleLogger, {unErrorJson} from '../../json-console-logger.js';
@@ -7,14 +8,22 @@ import {
   PREVIOUS_YEAR_RETURN_NOTIFY_TEMPLATE_ID,
   NEVER_SUBMITTED_RETURN_NOTIFY_TEMPLATE_ID,
   EXPIRED_RECENTLY_NO_RETURN_NOTIFY_TEMPLATE_ID,
-  LICENSING_REPLY_TO_NOTIFY_EMAIL_ID
+  LICENSING_REPLY_TO_NOTIFY_EMAIL_ID,
+  EXPIRED_RECENTLY_NO_RENEWALS_NOTIFY_TEMPLATE_ID
 } from '../../notify-template-ids.js';
 
-const {Registration, Return} = database;
+const {Registration, Return, Renewal} = database;
 
 const setReturnReminderEmailDetails = (registration) => ({
   id: registration.id,
   lhName: registration.fullName
+});
+
+const setRenewalReminderEmailDetails = (registration) => ({
+  id: registration.id,
+  lhName: registration.fullName,
+  expiry: registration.expiryDate,
+  hasMeatBaits: registration.meatbaits
 });
 
 const setPreviousYearReturnReminderEmailDetails = (registration) => ({
@@ -32,6 +41,30 @@ const setPreviousYearReturnReminderEmailDetails = (registration) => ({
  * @param {string} notifyTemplate The Notify template to use for the email.
  */
 const sendReturnReminderEmail = async (emailDetails, emailAddress, notifyTemplate) => {
+  if (config.notifyApiKey) {
+    try {
+      const notifyClient = new NotifyClient.NotifyClient(config.notifyApiKey);
+
+      // Send the email via notify.
+      await notifyClient.sendEmail(notifyTemplate, emailAddress, {
+        personalisation: emailDetails,
+        emailReplyToId: LICENSING_REPLY_TO_NOTIFY_EMAIL_ID
+      });
+    } catch (error) {
+      jsonConsoleLogger.error(unErrorJson(error));
+      throw error;
+    }
+  }
+};
+
+/**
+ * Send reminder email to applicant informing them that they can renew their application
+ *
+ * @param {string} emailDetails The details to use in personalisation of email.
+ * @param {any} emailAddress The email address of the recipient.
+ * @param {string} notifyTemplate The Notify template to use for the email.
+ */
+const sendRenewalReminderEmail = async (emailDetails, emailAddress, notifyTemplate) => {
   if (config.notifyApiKey) {
     try {
       const notifyClient = new NotifyClient.NotifyClient(config.notifyApiKey);
@@ -131,6 +164,30 @@ const ScheduledController = {
 
       promises.push(
         sendReturnReminderEmail(emailDetails, registration.emailAddress, EXPIRED_RECENTLY_NO_RETURN_NOTIFY_TEMPLATE_ID)
+      );
+      sentCount++;
+    }
+
+    await Promise.all(promises);
+
+    return sentCount;
+  },
+
+  async sendExpiredNoRenewalsReminder(registrations) {
+    // A count of the number of emails sent.
+    let sentCount = 0;
+
+    const promises = [];
+
+    for (const registration of registrations) {
+      const emailDetails = setRenewalReminderEmailDetails(registration);
+
+      promises.push(
+        sendRenewalReminderEmail(
+          emailDetails,
+          registration.emailAddress,
+          EXPIRED_RECENTLY_NO_RENEWALS_NOTIFY_TEMPLATE_ID
+        )
       );
       sentCount++;
     }
