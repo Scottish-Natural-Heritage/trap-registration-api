e065f3ba0799cd1cc3d915e91914e8de964fdacf
Rory Anderson <rory.anderson@nature.scot>
2024-12-09 15:22:00 +0000

    Add migration to populate trapId and registrationType column

---
+++ util/db/migrations/20241209143417-populate-trap-id-and-registration-type.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface) =>
+    queryInterface.sequelize.transaction(async (t) => {
+      const tableName = process.env.NODE_ENV === 'production' ? 'licensing.traps."Registrations"' : '"Registrations"';
+
+      await queryInterface.sequelize.query(
+        `
+				UPDATE ${tableName}
+				SET "trapId" = "id", "registrationType" = 'Initial'
+				WHERE "registrationType" IS NULL; 
+      `,
+        {transaction: t}
+      );
+    }),
+
+  down: async (queryInterface) =>
+    queryInterface.sequelize.transaction(async (t) => {
+      const tableName = process.env.NODE_ENV === 'production' ? 'licensing.traps."Registrations"' : '"Registrations"';
+
+      await queryInterface.sequelize.query(
+        `
+				UPDATE ${tableName}
+				SET "trapId" = NULL, "registrationType" = NULL; 
+      `,
+        {transaction: t}
+      );
+    })
+};
